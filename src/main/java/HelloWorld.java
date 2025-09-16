502a68596da24309a07494a7b1c5075005bb4005
Laura <laura.biroth@regnology.net>
2025-09-16 11:18:52 +0000

    Add real unit tests

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,8 @@
-
 public class HelloWorld {
 public static void main(String[] args) {
 // Prints "Hello, World" to the terminal window.
-System.out.println("Hallo Welt");
+HelloWorldHelper helper = new HelloWorldHelper ();
+System.out.println(helper.useEnglish());
 }
 }
+
